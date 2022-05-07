da5dadfaad8cd6ebf50a7548dbb00d1cdc081dae
Pushkarev Anton <pushkarevantona@gmail.com>
2022-05-08 00:19:49 +0300

    v.0.6 Users permissions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,7 +130,7 @@ DJOSER = {
     },
     'HIDE_USERS': False,
     'PERMISSIONS': {
-        'user': ['rest_framework.permissions.AllowAny'],
+        'user': ['rest_framework.permissions.IsAuthenticated'],
         'user_list': ['rest_framework.permissions.AllowAny'],
     },
 }
