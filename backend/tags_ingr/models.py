ea365854dd479ad0e84bca65904deb01634bd6b3
Pushkarev Anton <pushkarevantona@gmail.com>
2022-05-09 23:58:44 +0300

    v.0.12 custom admin + data in DB

--- backend/tags_ingr/models.py
+++ backend/tags_ingr/models.py
@@ -22,7 +22,19 @@ class Tag(models.Model):
         ]
     )
 
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=200)
     measurement_unit = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from tags_ingr.models import Ingredient, Tag
 
 from .models import User
 
+
+class IngredientAdmin(admin.ModelAdmin):
+    """Кастомная админка для модели Ingredient."""
+    list_display = (
+        'pk',
+        'name',
+        'measurement_unit',
+    )
+    list_editable = ('name',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class TagAdmin(admin.ModelAdmin):
+    """Кастомная админка для модели Tag."""
+    list_display = (
+        'pk',
+        'name',
+        'color',
+        'slug',
+    )
+    list_editable = ('name', 'color', 'slug')
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Tag)
-admin.site.register(Ingredient)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
