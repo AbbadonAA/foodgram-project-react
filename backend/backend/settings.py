aa188bf258168f627267fa2b06ac36706d80b280
Pushkarev Anton <pushkarevantona@gmail.com>
2022-05-14 23:43:22 +0300

    v.0.15.9 Pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,7 +118,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.LimitOffsetPagination',
+        'api.pagination.LimitPagination',
     'PAGE_SIZE': 6,
 }
 
